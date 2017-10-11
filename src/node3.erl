bdba072165e5bdf279074bda094c4e91987e2733
Nikhil Nadig <nikhilnadig28@gmail.com>
2017-10-11 17:53:39 +0200

    Minor Bug fixes. Node3 added
    Node3 doesn't work

--- src/node3.erl
+++ src/node3.erl
@@ -1,105 +1,115 @@
--module (node2).
--export([start/1, start/2, init/2, node/4, stabilize/3, stabilize/1]).
+-module (node3).
+-export([start/1, start/2, init/2, node/5, stabilize/4, stabilize/1]).
 -define('Stabilize',500).
 -define('Timeout',1000).
 
-node(Id, Predecessor, Successor , Store) ->
+node(Id, Predecessor, Successor , Store, Next) ->
 	receive
 		{key, Qref, Peer} ->
 			Peer ! {Qref, Id},
-			node(Id, Predecessor, Successor , Store);
+			node(Id, Predecessor, Successor , Store, Next);
 		{notify, New } ->
-			{Pred,NewStore} = notify(New, Id, Predecessor,Store),
-			node(Id, Pred, Successor , NewStore);
+			{Pred,NewStore} = notify(New, Id, Predecessor, Store),
+			node(Id, Pred, Successor , NewStore, Next);
 		{request, Peer} ->
-			request(Peer, Predecessor),
-			node(Id, Predecessor, Successor , Store);
-		{status, Pred } ->
-			Succ = stabilize(Pred, Id, Successor),
-			node(Id, Predecessor, Succ , Store);
+			request(Peer, Predecessor, Successor),
+			node(Id, Predecessor, Successor , Store, Next);
+		{status, Pred, Nx} ->
+			{Succ, Nxt} = stabilize(Pred, Id, Successor, Nx),
+			node(Id, Predecessor, Succ, Store, Nxt);
 		stabilize ->
 			stabilize(Successor),
-			node(Id, Predecessor, Successor , Store);
+			node(Id, Predecessor, Successor , Store, Next);
 		probe ->
 			create_probe(Id, Successor),
-			node(Id, Predecessor, Successor , Store);
+			node(Id, Predecessor, Successor , Store, Next);
 		{probe, Id, Nodes, T} ->
 			remove_probe(T, Nodes),
-			node(Id, Predecessor, Successor , Store);
+			node(Id, Predecessor, Successor , Store, Next);
 		{probe, Ref, Nodes, T} ->
 			forward_probe(Ref, T, Nodes, Id, Successor),
-			node(Id, Predecessor, Successor , Store);
+			node(Id, Predecessor, Successor , Store, Next);
 		status ->
 			io:format("Node: ~w   Predecessor: ~w Successor: ~w  Storage: ~w~n", [Id, Predecessor, Successor, Store]),
-			node(Id, Predecessor, Successor, Store);
+			node(Id, Predecessor, Successor, Store, Next);
 		{add, Key, Value}->
 			self() ! {add, Key, Value, self(), self()};
 		{add, Key, Value, Qref, Client} ->
 			Added = add(Key, Value, Qref, Client, Id, Predecessor, Successor, Store),
-			node(Id, Predecessor, Successor, Added);
+			node(Id, Predecessor, Successor, Added, Next);
 		{lookup, Key, Qref, Client} ->
 			lookup(Key, Qref, Client, Id, Predecessor, Successor, Store),
-			node(Id, Predecessor, Successor, Store);
+			node(Id, Predecessor, Successor, Store, Next);
 		{handover, Elements} ->
 			Merged = storage:merge(Store, Elements),
-			node(Id, Predecessor, Successor, Merged);
+			node(Id, Predecessor, Successor, Merged, Next);
+		{'DOWN', Ref, process, _, _} ->
+			{Pred, Succ, Nxt} = down(Ref, Predecessor, Successor, Next),
+			node(Id, Pred, Succ, Store, Nxt);
 		Error ->
 			io:format("Error! ~w~n", [Error]),
-			node(Id,Predecessor,Successor , Store)
+			node(Id,Predecessor,Successor , Store, Next)
 	end.
 
 
 
-stabilize(Pred, Id, Successor) ->
-	{Skey, Spid} = Successor,
+stabilize(Pred, Id, Successor, Nx) ->
+	{Skey, SRef, Spid} = Successor,
 	case Pred of
 		nil ->
 			Spid ! {notify, {Id, self()}},
-			Successor;
+			{Successor,Nx};
 		{Id, _} ->
-			Successor;
+			{Successor,Nx};
 		{Skey, _} ->
 			Spid ! {notify, {Id, self()}},
-			Successor;
+			{Successor,Nx};
 		{Xkey, Xpid} ->			
 			case key:between(Xkey, Id, Skey) of
 				true ->
 					Xpid ! {notify, {Id, self()}},
-					{Xkey, Xpid};
+					drop(SRef),
+					Xref = monitor(Xpid),
+					self() ! stabilize,
+					{{Xkey,Xref, Xpid}, Successor};
 				false ->
 					Spid ! {notify, {Id, self()}},
-					Successor
+					{Successor,Nx}
 			end
 	end.
+
 
 schedule_stabilize() ->
 	timer:send_interval(?Stabilize, self(), stabilize).
 
-stabilize({_, Spid}) ->
+stabilize({_, _, Spid}) ->
 	Spid ! {request, self()}.
 
-request(Peer, Predecessor) ->
+request(Peer, Predecessor, Next) ->
 	case Predecessor of
 		nil ->
-			Peer ! {status, nil};
-		{Pkey, Ppid} ->
-			Peer ! {status, {Pkey, Ppid}}
+			Peer ! {status, nil, Next};
+		{Pkey, _ , Ppid} ->
+			Peer ! {status, {Pkey, Ppid}, Next}
 	end.
 
-notify({Nkey, Npid}, Id, Predecessor , Store) ->
-	case Predecessor of
-		nil ->
-			Keep = handover(Id, Store , Nkey , Npid),
-			{{Nkey,Npid},Keep};
-		{Pkey, _} ->
-			case key:between(Nkey, Pkey, Id) of
-				true ->
-					Keep = handover(Id, Store , Nkey , Npid),
-					{{Nkey,Npid},Keep};
-				false ->
-					{Predecessor,Store}
-			end
-	end.
+notify({Nkey, Npid}, Id, Predecessor, Store) ->
+    case Predecessor of
+        nil ->
+            Keep = handover(Id, Store, Nkey, Npid), 
+            Nref = monitor(Npid),
+            {{Nkey, Nref, Npid}, Keep};
+        {Pkey,Pref, Pid} ->
+            case key:between(Nkey, Pkey, Id) of
+                true ->
+                    Keep = handover(Id, Store, Nkey, Npid),
+                    drop(Pref),
+                    NRef = monitor(Npid),
+                    {{Nkey, NRef , Npid}, Keep};
+                false ->
+                    {Predecessor, Store}
+            end
+    end.
 
 
 
@@ -116,17 +126,19 @@ init(Id, Peer) ->
 	{ok, Successor} = connect(Id, Peer),
 	schedule_stabilize(),
 	Store = storage:create(),
-	node(Id, Predecessor, Successor, Store).
+	Next = nil,
+	node(Id, Predecessor, Successor, Store, Next).
 
 
 connect(Id, nil) ->
-	{ok, {Id,self()}};
+	{ok, {Id, nil, self()}};
 
 connect(Id, Peer) ->
 	Qref = make_ref(),
 	Peer ! {key, Qref, self()},
 	receive
 		{Qref, Skey} ->
+			Ref = monitor(Peer),
 			io:format("Node ~w started.~n",[Id]),
 			{ok,{Skey,Peer}}
 	after 
@@ -154,6 +166,7 @@ add(Key, Value, Qref, Client, Id, {Pkey, _}, {_, Spid}, Store) ->
 		case key:between(Key, Pkey, Id) of
 			true ->
 				Client ! {Qref, ok},
+				io:format("Adding Key ~w and Value ~w~n",[Key, Value]),
 				storage:add(Key, Value, Store);
 			false ->
 				Spid ! {add, Key, Value, Qref, Client},
@@ -175,3 +188,22 @@ handover(Id, Store, Nkey, Npid) ->
 		{Keep, Leave} = storage:split(Nkey, Id, Store),
 		Npid ! {handover, Leave},
 		Keep.
+
+monitor(Pid) ->
+	erlang:monitor(process, Pid).
+
+drop(nil) ->
+	ok;
+
+drop(Pid) ->
+	erlang:demonitor(Pid, [flush]).
+
+down(Ref, {_, Ref, _}, Successor, Next) ->
+	io:format("Predecessor died. ~n"),
+	{nil, Successor, Next};
+
+down(Ref, Predecessor, {_, Ref, _}, {Nkey,_, Npid}) ->	
+	self() ! stabilize,
+	Nref = monitor(Npid),
+	io:format("Successor died.~n"),
+	{Predecessor, {Nkey, Nref, Npid}, nil}.
